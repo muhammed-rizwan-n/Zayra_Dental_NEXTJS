da2580d6af9d7a475c3eaac532a178b0eb4e5ae3
muhammed-rizwan-n <148784103+muhammed-rizwan-n@users.noreply.github.com>
2025-07-04 14:36:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./critical.css";
 import "./globals.css";
-//import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import AOSInitializer from "../components/AOSInitializer";
-
-// Lazy load Bootstrap CSS to improve initial page load
-import dynamic from "next/dynamic";
 
 // Load Bootstrap asynchronously
 if (typeof window !== "undefined") {
