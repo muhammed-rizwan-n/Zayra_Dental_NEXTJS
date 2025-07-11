9f7d091ca79973ad949229928946c4b82d494542
muhammed-rizwan-n <muhdrizwan021@gmail.com>
2025-07-11 23:21:02 +0530

    Update in sitemap setup

---
+++ app/sitemap.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://zayra-dental-nextjs.vercel.app";
+  const urls = [
+    "",
+    "about-us",
+    "services",
+    "services/teeth-whitening",
+    "services/dental-implants",
+    "services/dental-aligners",
+    "services/composite-and-veneers",
+    "services/general-dentistry",
+    "services/root-canal",
+    "services/emergency-dental-care",
+    "pricing",
+    "gallery",
+    "contact",
+    "appointment",
+  ];
+ return urls.map((path) => ({
+    url: `${baseUrl}/${path}`,
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1,
+  }));
+}
