5f063a83bd52179dcaeaef24a4767c1216c0ff22
muhammed-rizwan-n <muhdrizwan021@gmail.com>
2025-07-19 13:59:38 +0530

    updates in cpanel and next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'standalone',
+  reactStrictMode: true,
   images: {
     formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
